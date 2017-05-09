a4f51ec4be0288bb053cc988d5dde2988daa1693
dbrosy <dbrosy@hotmail.com>
2017-05-09 17:28:16 +1000

    fix for ctrl+c

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,9 +2,18 @@
 var http = require('http');
 
 // Configure our HTTP server to respond with "hello world" to all requests.
-var server = createServer(function (requesrt, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Welcome to Node.js!\n");
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<!DOCTYPE html>");
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title>Hello World Page</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("Hello World!");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
 });
 
 // Listen on port 3000, IP default to 127.0.0.1
@@ -12,3 +21,9 @@ server.listen(3000);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://...:3000");
+
+
+// fix for ctrl+c not working
+process.on('SIGINT', function () {
+  process.exit(1);
+});
